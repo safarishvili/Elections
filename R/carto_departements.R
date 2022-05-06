8d39a2d30457606aa0236d13a6cc8d03b89bc692
safarishvili <90871766+safarishvili@users.noreply.github.com>
2022-05-07 01:30:03 +0300

    Update carto_departements.R
    2 graphiques

--- R/carto_departements.R
+++ R/carto_departements.R
@@ -25,3 +25,33 @@ dept %>%
     labs(title = "Carte des départements de France métropolitaine en 2014")
 
 
+#Importation de la BDD "ESADA_W"
+ESADA_W<-read.csv(C:/Users/Irina/documents for R/ESADA_W.csv")
+
+#les calculs nécessaires pour construire heatmap ESADA_W
+
+h.m<-ESADA_W %>% 
+  group_by(region,classe) %>% 
+  summarise(N=n()) %>% 
+  mutate(Sum=sum(N), prop=N/Sum)
+
+#visualisation heatmap
+
+h.m %>% 
+  ggplot(aes(classe,region))+geom_tile(aes(fill=prop))+
+  geom_text(aes(label=scales::percent(prop, accuracy = 1)))+
+  theme_minimal()+
+  scale_fill_gradient(low="#E6E6FA", high="#CD5C5C" )
+                  
+#les modifications et calculs nécessaires pour creer bubble chart
+
+b.ch<-ESADA_W %>% 
+  group_by(region,classe) %>% 
+  summarise(N=n()) %>% 
+  mutate(Sum=sum(N), prop=N/Sum) 
+                  
+#le brouillon de la visualisation de bubble chart
+b.ch %>% 
+  ggplot(aes(region,prop, size=prop, color=classe))+
+  geom_point()+theme_minimal()+
+  geom_text(aes(label=scales::percent(prop, accuracy = 1)))
